9ccbe8f6f0b20a926a034b2cde6cf366df2b6ab3
unknown <ryanadalal@gmail.com>
2022-01-29 21:44:12 -0500

    adds x's and o's alternatly, playable on multiple devices

--- index.js
+++ index.js
@@ -7,18 +7,22 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+turn = 'x';
+
 app.use(express.static(__dirname + '/client'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
 io.on('connection', socket => {
+  io.emit('reset');
   console.log("Connection with user")
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
   socket.on('box', (num) => {
-      io.emit('madeMove', { spot: num, xoro: 'x' }); 
+      io.emit('madeMove', { spot: num, xoro: turn }); 
+      turn = (turn == 'x') ? 'o' : 'x';
   });
 });
 
